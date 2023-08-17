cd56b08f453db8d661c597fbdd9c68da99c043fe
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2023-08-17 16:06:49 +0200

    adding-phonebook

---
+++ src/components/ContactList/index.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ContactListElement from './ContactListElement';
+import PropTypes from 'prop-types';
+
+class ContactList extends Component {
+  render() {
+    const { contacts, onClick } = this.props;
+    return (
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            <ContactListElement
+              onClick={onClick}
+              name={contact.name}
+              number={contact.number}
+              id={contact.id}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  onClick: PropTypes.func,
+};
+
+export default ContactList;
